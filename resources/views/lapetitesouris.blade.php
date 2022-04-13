fe9a454caa6bed9e16c5866f908ac09888d08c77
kant1burger <quentin.loiseleux@labom2iformation.fr>
2022-04-13 16:32:26 +0200

    ajout essais

--- resources/views/lapetitesouris.blade.php
+++ resources/views/lapetitesouris.blade.php
@@ -1,1 +1,3 @@
 @extends('layouts.base')
+
+<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat laborum recusandae unde. Expedita iste necessitatibus, fugit dicta quis qui laudantium aspernatur assumenda sunt nam soluta, porro consequatur odio quibusdam ducimus?</p>
