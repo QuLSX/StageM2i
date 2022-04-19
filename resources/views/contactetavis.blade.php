8009e4493b51f3a379fcfcca83d05c699928fa85
kant1burger <quentin.loiseleux@labom2iformation.fr>
2022-04-19 16:32:37 +0200

    test css mardi

--- resources/views/contactetavis.blade.php
+++ resources/views/contactetavis.blade.php
@@ -1,1 +1,30 @@
 @extends('layouts.base')
+<div class="allpage">
+    <div class="alltext">
+
+        <h1 class="titrecontact">Nous Contacter</h1>
+
+        <div class="fiches">
+            <div class="petitesouris">
+                <h4>La Petite Souris</h4>
+                    <p> Adresse Mail : <a href="mailto:emailexemple@mail.com"> emailexemple@mail.com </a> </p>
+                    <p> Téléphone : 07.68.38.76.95 </p>
+                    <p> Notre page Facebook : <a href="https://m.facebook.com/lapetitesouris02000/">La Petite Souris</a></p>
+            </div>
+
+            <div class="cherryink">
+                <h4>Cherry ink Tattoo</h4>
+                    <p> Adresse Mail : <a href="mailto:emailexemple@mail.com"> emailexemple@mail.com </a> </p>
+                    <p> Téléphone : 06.18.24.05.42 </p>
+                    <p> Notre page Facebook : <a href="https://www.facebook.com/cherryinktattoo">Cherry Ink Tattoo</a></p>
+            </div>
+        </div>
+
+        <div class="adresse">
+            <p>Vous pouvez nous retrouver à l'adresse suivante :</p>
+            <p>17 rue Eugène Leduc, 02000 LAON</p>
+
+            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2587.555080246832!2d3.6292242156996344!3d49.56841757936334!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47e84b48c9d0ed25%3A0x76a94f437218cd8a!2s17%20Rue%20Eug%C3%A8ne%20Leduc%2C%2002000%20Laon!5e0!3m2!1sfr!2sfr!4v1650374921019!5m2!1sfr!2sfr" width="800" height="600" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+        </div>
+    </div>
+</div>
--- resources/views/lapetitesouris.blade.php
+++ resources/views/lapetitesouris.blade.php
@@ -2,3 +2,11 @@
 
 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat laborum recusandae unde. Expedita iste necessitatibus, fugit dicta quis qui laudantium aspernatur assumenda sunt nam soluta, porro consequatur odio quibusdam ducimus?</p>
 
+
+<h1>La Petite Souris</h1>
+
+<ul>
+    <li>Piercings</li>
+    <li>Vente de Bijoux</li>
+    <li>Pose de strass dentaire</li>
+</ul>
